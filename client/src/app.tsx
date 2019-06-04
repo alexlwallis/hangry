209ecffe2fee7b2467e7c40ca24950dcf6227ea8
Alex Wallis <aldwallis@Alexs-MacBook-Air.local>
2019-06-03 21:59:42 -0700

    Created textbox that alters state

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,35 +1,71 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-class App extends React.Component {
+type MyState = {
+   latitude: Number,
+   longitude: Number,
+   needsBox: Boolean,
+   location: String
+}
+
+class App extends React.Component<{}, MyState > {
   constructor(props:any){
     super(props);
     this.state = {
       latitude: 0,
-      longitude: 0
+      longitude: 0,
+      needsBox: false,
+      location: ''
     }
     this.geo = this.geo.bind(this);
+    this.changeCity = this.changeCity.bind(this);
+  }
+
+  componentDidMount(){
+    this.geo();
   }
 
 
   geo(){
+    //Use first arg in callback to update state w/ longitude+latitude+whether or input is needed if user allows geolocation.
     navigator.geolocation.getCurrentPosition(pos => {
       console.log(pos.coords.latitude, pos.coords.longitude)
       let lat: Number = pos.coords.latitude;
       let lon: Number = pos.coords.longitude
       this.setState({
         latitude: lat,
-        longitude: lon
+        longitude: lon,
+        needsBox: false,
       })
+    //If there is an error w/ geolocation, we change state, so needsBox is true.
+    }, (err) => {
+      if (err) {
+        this.setState({
+          needsBox: true
+        })
+      }
+    })
+  }
+
+  changeCity(event: any){
+    this.setState({
+      location: event.target.value
     })
   }
 
 
   render() {
+    //We check if needsBox is true, if it is then we start w/ a form and text box.
     return (
       <div>
-        Hello World! React + TypeScript!
-        <button onClick={this.geo}>Press me.</button>
+        {this.state.needsBox
+          // ? <form action="/location" method="post" encType="multipart/form-data">
+            ? <div>
+                <label>What city?</label>
+                <input type='text' name='city' placeholder="City?" onChange={this.changeCity}></input>
+            </div>
+          : <div/>
+        }
       </div>
     )
   }
--- server/server.js
+++ server/server.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 1234
+const bodyParser = require("body-parser");
 
 app.use('/node_modules', express.static('node_modules'));
 app.use('/', express.static(path.resolve('client', 'dist')));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+app.post('/cityLocation', (req, res) => {
+  res.status(200);
+  let city = req.body.city;
+  console.log(req.body.city);
+  res.redirect('/')
+})
+
+app.get('/cityLocation', (req, res) => {
+  res.status(200);
+
+})
 
 app.listen(port, () => console.log(`Express server running at ${port}`))
